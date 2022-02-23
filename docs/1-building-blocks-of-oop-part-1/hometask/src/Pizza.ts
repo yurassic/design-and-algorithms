5187131bf47f9fa7400f2e33e3db69b0bea85481
yurassic <ynyay@assurance.com>
2022-02-24 01:09:48 +0200

    Add pizza

--- docs/1-building-blocks-of-oop-part-1/hometask/src/Pizza.ts
+++ docs/1-building-blocks-of-oop-part-1/hometask/src/Pizza.ts
@@ -1,1 +1,31 @@
-// your code goes here
+import { Consumable } from "./Consumable";
+
+export class Pizza extends Consumable {
+  numberOfSlices: number;
+  slicesEaten: number;
+
+  constructor(
+    name: string,
+    value: number,
+    weight: number,
+    spoiled: boolean,
+    numberOfSlices: number
+  ) {
+    super(name, value, weight, spoiled);
+    this.numberOfSlices = numberOfSlices;
+  }
+
+  eat(): string {
+    if (this.slicesEaten > this.numberOfSlices) {
+      this.slicesEaten++;
+
+      if (this.slicesEaten >= this.numberOfSlices) {
+        this.setConsumed(true);
+      }
+
+      return "You eat slice of pizza";
+    } else {
+      return "";
+    }
+  }
+}
