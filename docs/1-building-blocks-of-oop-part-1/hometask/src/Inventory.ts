6fab9f573dfd953288b17f6028b820fd91f05b4e
yurassic <ynyay@assurance.com>
2022-02-24 00:33:02 +0200

    Add more classes

--- docs/1-building-blocks-of-oop-part-1/hometask/src/Inventory.ts
+++ docs/1-building-blocks-of-oop-part-1/hometask/src/Inventory.ts
@@ -1,1 +1,18 @@
-// your code goes here
+import { Item } from "./Item";
+export class Inventory {
+  items: Item[];
+
+  addItem(item: Item): void {
+    this.items.push(item);
+  }
+
+  sort(): void {
+    this.items.sort((a, b) => (a.value > b.value ? 1 : -1));
+  }
+
+  // TODO: Inventory.sort(comparator: ItemComparator)
+
+  toString(): string {
+    return this.items.map((item) => item.name).join(", ");
+  }
+}
--- docs/1-building-blocks-of-oop-part-1/hometask/src/Sword.ts
+++ docs/1-building-blocks-of-oop-part-1/hometask/src/Sword.ts
@@ -1,1 +1,23 @@
-// your code goes here
+import { Weapon } from "./Weapon";
+
+export class Sword extends Weapon {
+  constructor(
+    ...args: [
+      baseDamage: number,
+      baseDurability: number,
+      damageModifier: number,
+      durabilityModifier: number,
+      name: string,
+      value: number,
+      weight: number
+    ]
+  ) {
+    super(...args);
+    this.name = "sword";
+  }
+
+  polish(): void {
+    // TODO:
+    this.damageModifier = this.MODIFIER_CHANGE_RATE;
+  }
+}
